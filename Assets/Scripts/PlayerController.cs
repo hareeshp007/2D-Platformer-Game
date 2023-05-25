5a829b920355ac16c63b2a0eca7dfda6bebbb8ad
hareeshp007 <hareeshpadinjath@gmail.com>
2023-05-25 12:45:27 +0530

    Player animation controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator animator;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("speed", Mathf.Abs(speed));
+        Vector3 scale=transform.localScale;
+        if (speed < 0)
+        {
+            scale.x=-1*Mathf.Abs(scale.x);
+        }
+        else if (speed > 0)
+        {
+            scale.x = 1 * Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision detected" +collision.gameObject);
     }
 }
