41107390901764536769bd193862aab20c005f4c
hareeshp007 <hareeshpadinjath@gmail.com>
2023-05-26 12:04:57 +0530

    Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,41 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    public float speed;
+
+    public bool Onground;
 
     private void Update()
     {
-        bool isjump;
         bool cntrl = Input.GetKey(KeyCode.LeftControl);
-        animator.SetBool("Iscrouch", cntrl);
+        bool jump = Input.GetKey(KeyCode.Space);
+        Vector3 scale = transform.localScale;
+        Vector3 pos=transform.position;
+        //animator.SetFloat("vertical",vert);  
+        float Hori = Input.GetAxisRaw("Horizontal");
         float vert = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("vertical",vert);
-        isjump = true;
-        /*if (vert>0 && isjump == true)
+        PlayerMovementAnimation(scale, Hori, vert, cntrl, jump);
+        PlayerMovement(pos, Hori, vert);
+    }
+
+    private void PlayerMovement(Vector3 pos, float Hori, float vert)
+    {
+        pos.x += Hori * speed * Time.deltaTime;
+        transform.position = pos;
+    }
+
+    private void PlayerMovementAnimation(Vector3 scale, float Hori, float vert,bool cntrl,bool jump)
+    {
+        animator.SetBool("Iscrouch", cntrl);
+        animator.SetFloat("speed", Mathf.Abs(Hori));
+        if (jump && Onground)
         {
-            isjump = false;
             animator.SetTrigger("Isjumping");
-            isjump = tru;
-        }*/
-        
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("speed", Mathf.Abs(speed));
-        Vector3 scale=transform.localScale;
-        if (speed < 0)
-        {
-            scale.x=-1*Mathf.Abs(scale.x);
         }
-        else if (speed > 0)
+        if (Hori < 0)
+        {
+            scale.x = -1 * Mathf.Abs(scale.x);
+        }
+        else if (Hori > 0)
         {
             scale.x = 1 * Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if(collision != null)
+        {
+            Onground = true;
+        }
+        else {
+            Onground = false;
+        }
         Debug.Log("collision detected" +collision.gameObject);
     }
 }
