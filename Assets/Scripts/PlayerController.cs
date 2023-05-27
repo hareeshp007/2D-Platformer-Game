c81143313464a393d8627c8df0b7c026f55662ac
hareeshp007 <hareeshpadinjath@gmail.com>
2023-05-27 11:42:30 +0530

    jump mechanism

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,41 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static UnityEngine.EventSystems.EventTrigger;
 
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-    public float speed;
-
+    public float speed,jumppower;
+    private Rigidbody2D rb2d;
     public bool Onground;
 
+    public Transform feetpos;
+    public float checkRadius;
+    public LayerMask Ground;
+    
+    private void Awake()
+    {
+        rb2d=GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
+        Onground=Physics2D.OverlapCircle(feetpos.position,checkRadius,Ground);
+        animator.SetBool("OnGround", Onground);
         bool cntrl = Input.GetKey(KeyCode.LeftControl);
         bool jump = Input.GetKey(KeyCode.Space);
         Vector3 scale = transform.localScale;
-        Vector3 pos=transform.position;
-        //animator.SetFloat("vertical",vert);  
+        Vector3 pos=transform.position; 
         float Hori = Input.GetAxisRaw("Horizontal");
         float vert = Input.GetAxisRaw("Vertical");
-        PlayerMovementAnimation(scale, Hori, vert, cntrl, jump);
-        PlayerMovement(pos, Hori, vert);
+        PlayerMovementAnimation( Hori, vert, cntrl, jump);
+        PlayerMovement(pos, Hori,vert, jump, scale);
     }
 
-    private void PlayerMovement(Vector3 pos, float Hori, float vert)
+    private void PlayerMovement(Vector3 pos, float Hori, float vert, bool jump, Vector3 scale)
     {
+
         pos.x += Hori * speed * Time.deltaTime;
         transform.position = pos;
-    }
-
-    private void PlayerMovementAnimation(Vector3 scale, float Hori, float vert,bool cntrl,bool jump)
-    {
-        animator.SetBool("Iscrouch", cntrl);
-        animator.SetFloat("speed", Mathf.Abs(Hori));
-        if (jump && Onground)
+        if (vert>0 && Onground)
         {
+            Debug.Log(jump + "+ " + Onground);
             animator.SetTrigger("Isjumping");
+            rb2d.velocity = Vector2.up * jumppower;
+            //rb2d.AddForce(new Vector2(0f, jumppower), ForceMode2D.Impulse);
+            Onground = false;
         }
         if (Hori < 0)
         {
@@ -47,17 +56,28 @@ public class PlayerController : MonoBehaviour
             scale.x = 1 * Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+    }
+
+    private void PlayerMovementAnimation( float Hori, float vert,bool cntrl,bool jump)
+    {
+        
+        if (Onground) {
+            animator.SetBool("Iscrouch", cntrl);
+            animator.SetFloat("speed", Mathf.Abs(Hori));    
+        }
+        
+        
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision != null)
+        if(collision.gameObject.CompareTag("ground"))
         {
-            Onground = true;
+            //Onground = true;
         }
-        else {
-            Onground = false;
-        }
-        Debug.Log("collision detected" +collision.gameObject);
+        
+        Debug.Log("collision detected" +collision.gameObject.tag);
     }
 }
